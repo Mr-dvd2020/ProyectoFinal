7dcb5859047a200b19843690481441d6a1afc219
willian salas ccolque <williansalasccolqquee@gmail.com>
2021-03-09 08:25:44 -0500

    ventana Almacenero, botones y funciones

--- Menu/Almacenero.Designer.cs
+++ Menu/Almacenero.Designer.cs
@@ -30,18 +30,18 @@ namespace Menu
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Almacenero));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxCantidad = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.BtnModificar = new System.Windows.Forms.Button();
             this.BtnEliminar = new System.Windows.Forms.Button();
             this.BtnAgregar = new System.Windows.Forms.Button();
-            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtUnidad = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtId = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxPrecioUnitario = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.btcerrarAlmacenero = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -50,78 +50,91 @@ namespace Menu
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxCantidad
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 144);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 20);
-            this.textBox1.TabIndex = 38;
+            this.textBoxCantidad.Location = new System.Drawing.Point(317, 343);
+            this.textBoxCantidad.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxCantidad.Name = "textBoxCantidad";
+            this.textBoxCantidad.Size = new System.Drawing.Size(513, 38);
+            this.textBoxCantidad.TabIndex = 38;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(17, 147);
+            this.label4.Location = new System.Drawing.Point(45, 351);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.Size = new System.Drawing.Size(130, 32);
             this.label4.TabIndex = 37;
             this.label4.Text = "Cantidad";
             // 
             // BtnModificar
             // 
-            this.BtnModificar.Location = new System.Drawing.Point(338, 116);
+            this.BtnModificar.Location = new System.Drawing.Point(901, 277);
+            this.BtnModificar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.BtnModificar.Name = "BtnModificar";
-            this.BtnModificar.Size = new System.Drawing.Size(75, 23);
+            this.BtnModificar.Size = new System.Drawing.Size(200, 55);
             this.BtnModificar.TabIndex = 36;
             this.BtnModificar.Text = "Actualizar";
             this.BtnModificar.UseVisualStyleBackColor = true;
+            this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
             // 
             // BtnEliminar
             // 
-            this.BtnEliminar.Location = new System.Drawing.Point(338, 85);
+            this.BtnEliminar.Location = new System.Drawing.Point(901, 203);
+            this.BtnEliminar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.BtnEliminar.Name = "BtnEliminar";
-            this.BtnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEliminar.Size = new System.Drawing.Size(200, 55);
             this.BtnEliminar.TabIndex = 35;
             this.BtnEliminar.Text = "Listar";
             this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
             // 
             // BtnAgregar
             // 
-            this.BtnAgregar.Location = new System.Drawing.Point(338, 57);
+            this.BtnAgregar.Location = new System.Drawing.Point(901, 136);
+            this.BtnAgregar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.BtnAgregar.Name = "BtnAgregar";
-            this.BtnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.BtnAgregar.Size = new System.Drawing.Size(200, 55);
             this.BtnAgregar.TabIndex = 34;
             this.BtnAgregar.Text = "Añadir";
             this.BtnAgregar.UseVisualStyleBackColor = true;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
             // 
-            // txtApellidos
+            // txtUnidad
             // 
-            this.txtApellidos.Location = new System.Drawing.Point(119, 116);
-            this.txtApellidos.Name = "txtApellidos";
-            this.txtApellidos.Size = new System.Drawing.Size(195, 20);
-            this.txtApellidos.TabIndex = 33;
+            this.txtUnidad.Location = new System.Drawing.Point(317, 277);
+            this.txtUnidad.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtUnidad.Name = "txtUnidad";
+            this.txtUnidad.Size = new System.Drawing.Size(513, 38);
+            this.txtUnidad.TabIndex = 33;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(119, 87);
+            this.txtNombre.Location = new System.Drawing.Point(317, 207);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(195, 20);
+            this.txtNombre.Size = new System.Drawing.Size(513, 38);
             this.txtNombre.TabIndex = 32;
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(119, 59);
+            this.txtId.Location = new System.Drawing.Point(317, 141);
+            this.txtId.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(195, 20);
+            this.txtId.Size = new System.Drawing.Size(513, 38);
             this.txtId.TabIndex = 31;
+            this.txtId.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtId_KeyUp);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(17, 90);
+            this.label3.Location = new System.Drawing.Point(45, 215);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.Size = new System.Drawing.Size(123, 32);
             this.label3.TabIndex = 30;
             this.label3.Text = "Nombre:";
             // 
@@ -129,9 +142,10 @@ namespace Menu
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(17, 119);
+            this.label2.Location = new System.Drawing.Point(45, 284);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(114, 32);
             this.label2.TabIndex = 29;
             this.label2.Text = "Unidad:";
             // 
@@ -139,34 +153,38 @@ namespace Menu
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(17, 62);
+            this.label1.Location = new System.Drawing.Point(45, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(114, 32);
             this.label1.TabIndex = 28;
             this.label1.Text = "Código:";
             // 
-            // textBox2
+            // textBoxPrecioUnitario
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 172);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 20);
-            this.textBox2.TabIndex = 40;
+            this.textBoxPrecioUnitario.Location = new System.Drawing.Point(317, 410);
+            this.textBoxPrecioUnitario.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxPrecioUnitario.Name = "textBoxPrecioUnitario";
+            this.textBoxPrecioUnitario.Size = new System.Drawing.Size(513, 38);
+            this.textBoxPrecioUnitario.TabIndex = 40;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Location = new System.Drawing.Point(17, 175);
+            this.label5.Location = new System.Drawing.Point(45, 417);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.Size = new System.Drawing.Size(96, 32);
             this.label5.TabIndex = 39;
             this.label5.Text = "Precio";
             // 
             // btcerrarAlmacenero
             // 
-            this.btcerrarAlmacenero.Location = new System.Drawing.Point(150, 359);
+            this.btcerrarAlmacenero.Location = new System.Drawing.Point(400, 856);
+            this.btcerrarAlmacenero.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btcerrarAlmacenero.Name = "btcerrarAlmacenero";
-            this.btcerrarAlmacenero.Size = new System.Drawing.Size(132, 24);
+            this.btcerrarAlmacenero.Size = new System.Drawing.Size(352, 57);
             this.btcerrarAlmacenero.TabIndex = 41;
             this.btcerrarAlmacenero.Text = "Cerrar Sesion";
             this.btcerrarAlmacenero.UseVisualStyleBackColor = true;
@@ -177,53 +195,59 @@ namespace Menu
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(80)))), ((int)(((byte)(150)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 198);
+            this.dataGridView1.Location = new System.Drawing.Point(45, 472);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(396, 155);
+            this.dataGridView1.RowHeadersWidth = 102;
+            this.dataGridView1.Size = new System.Drawing.Size(1056, 370);
             this.dataGridView1.TabIndex = 42;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // lblAlmacenero
             // 
             this.lblAlmacenero.AutoSize = true;
             this.lblAlmacenero.BackColor = System.Drawing.Color.Transparent;
             this.lblAlmacenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAlmacenero.Location = new System.Drawing.Point(15, 13);
+            this.lblAlmacenero.Location = new System.Drawing.Point(40, 31);
+            this.lblAlmacenero.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblAlmacenero.Name = "lblAlmacenero";
-            this.lblAlmacenero.Size = new System.Drawing.Size(136, 25);
+            this.lblAlmacenero.Size = new System.Drawing.Size(317, 61);
             this.lblAlmacenero.TabIndex = 44;
             this.lblAlmacenero.Text = "Almacenero";
             // 
             // lblmensajeAlmacenero
             // 
             this.lblmensajeAlmacenero.AutoSize = true;
-            this.lblmensajeAlmacenero.Location = new System.Drawing.Point(86, 22);
+            this.lblmensajeAlmacenero.Location = new System.Drawing.Point(229, 52);
+            this.lblmensajeAlmacenero.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblmensajeAlmacenero.Name = "lblmensajeAlmacenero";
-            this.lblmensajeAlmacenero.Size = new System.Drawing.Size(0, 13);
+            this.lblmensajeAlmacenero.Size = new System.Drawing.Size(0, 32);
             this.lblmensajeAlmacenero.TabIndex = 43;
             // 
             // Almacenero
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(433, 390);
+            this.ClientSize = new System.Drawing.Size(1155, 930);
             this.Controls.Add(this.lblAlmacenero);
             this.Controls.Add(this.lblmensajeAlmacenero);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btcerrarAlmacenero);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxPrecioUnitario);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxCantidad);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.BtnModificar);
             this.Controls.Add(this.BtnEliminar);
             this.Controls.Add(this.BtnAgregar);
-            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtUnidad);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Almacenero";
             this.Text = "Almacenero";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -233,18 +257,18 @@ namespace Menu
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxCantidad;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button BtnModificar;
         private System.Windows.Forms.Button BtnEliminar;
         private System.Windows.Forms.Button BtnAgregar;
-        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtUnidad;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxPrecioUnitario;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btcerrarAlmacenero;
         private System.Windows.Forms.DataGridView dataGridView1;
--- Menu/Almacenero.cs
+++ Menu/Almacenero.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Menu
@@ -17,6 +17,12 @@ namespace Menu
     /// aea
     public partial class Almacenero : Form
     {
+        SqlConnection cn = new SqlConnection("server=DESKTOP-S92T72U ; database=Tienda ; integrated security = true");
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+        DataTable dt;
+
         public Almacenero(string nombre)
         {
             InitializeComponent();
@@ -28,6 +34,141 @@ namespace Menu
             Login inicio = new Login();
             inicio.Show();
             this.Close();
+        }
+
+        public void MostrarProductos(DataGridView dgv)
+        {
+
+            try
+            {
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter("Select * From Producto", cn);
+                da.Fill(dt);
+                dgv.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo llenar el DataGridView: " + ex.ToString());
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            MostrarProductos(dataGridView1);
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Producto SET Cantida=@Cantida, PrecioUnitario=@PrecioUnitario WHERE CodigoProducto=@CodigoProducto";
+            cn.Open();
+            cmd = new SqlCommand(query, cn);
+            cmd.Parameters.AddWithValue("@CodigoProducto", txtId.Text);
+            cmd.Parameters.AddWithValue("@PrecioUnitario", textBoxPrecioUnitario.Text);
+            cmd.Parameters.AddWithValue("@Cantida", textBoxCantidad.Text);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Se actualizo la informacion");
+            txtId.Clear();
+            textBoxPrecioUnitario.Clear();
+            textBoxCantidad.Clear();
+            txtId.Text = "";
+            txtNombre.Text = "";
+            txtUnidad.Text = "";
+            textBoxCantidad.Text = "";
+            textBoxPrecioUnitario.Text = "";
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            if (ProductoRegistrado(txtId.Text) == 0)
+            {
+                MessageBox.Show(agregar(txtId.Text, txtNombre.Text, txtUnidad.Text, textBoxCantidad.Text, textBoxPrecioUnitario.Text));
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtUnidad.Text = "";
+                textBoxCantidad.Text = "";
+                textBoxPrecioUnitario.Text= ""; 
+            }
+            else
+            {
+                cn.Close();
+                MessageBox.Show("Imposible de regitrar, Un Producto con este codigo ya existe");
+                txtId.Text = "";
+                txtNombre.Text = "";
+                txtUnidad.Text = "";
+                textBoxCantidad.Text = "";
+                textBoxPrecioUnitario.Text = "";
+            }
+            cn.Close();
+        }
+
+        public string agregar(string CodigoProducto, string Descripcion, string Unidad, string Cantidad, string PrecioUnitario)
+        {
+            string salida = "se creo el Producto correctamente";
+            try
+            {
+                cmd = new SqlCommand("insert into Producto(CodigoProducto,Descripcion,Unidad,Cantida,PrecioUnitario) values('" + CodigoProducto + "','" + Descripcion + "','" + Unidad + "'," + Cantidad + ","+ PrecioUnitario + ")", cn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception E)
+            {
+                salida = "No se pudo crear: " + E.ToString();
+            }
+            return salida;
+        }
+
+        public int ProductoRegistrado(string CodigoProducto)
+        {
+            int contador = 0;
+            try
+            {
+                cmd = new SqlCommand("Select * From Producto where CodigoProducto='" + CodigoProducto + "'", cn);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    contador++;
+                }
+                dr.Close();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("No se pudo consultar bien: " + Ex.ToString());
+            }
+            return contador;
+        }
+
+        private void txtId_KeyUp(object sender, KeyEventArgs e)
+        {
+            cn.Open();
+
+            cmd = cn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Producto where CodigoProducto like ('" + txtId.Text + "%')";
+            cmd.ExecuteNonQuery();
+
+            dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            cn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtId.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtNombre.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtUnidad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                textBoxCantidad.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                textBoxPrecioUnitario.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            }
+            catch { }
         }
     }
 }
